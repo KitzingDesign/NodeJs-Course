45c9c71c7a9f095241a039b126dd54a9990b5266
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2024-08-29 09:37:00 +0200

    Final code for fifth lecture

--- server.js
+++ server.js
@@ -4,30 +4,15 @@ const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandeler");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 // Custom middleware logger
 app.use(logger);
 
-//cross origin resource sharing
-const whitelist = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by cors"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
 app.use(cors(corsOptions));
 
-//Built in middleware to handle urlencoded data
+//Built in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json
@@ -35,12 +20,14 @@ app.use(express.json());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "/public")));
-app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
 //routes
 app.use("/", require("./routes/root"));
-app.use("/subdir", require("./routes/subdir"));
+app.use("/register", require("./routes/register"));
+
 app.use("/employees", require("./routes/api/employees"));
+
+app.use("/auth", require("./routes/auth"));
 
 app.all("*", (req, res) => {
   res.status(404);
